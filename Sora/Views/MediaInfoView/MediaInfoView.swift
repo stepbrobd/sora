508319452efcb8d383086317bfdbc74da026e545
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-03-12 18:05:17 +0100

    Update MediaInfoView.swift

--- Sora/Views/MediaInfoView/MediaInfoView.swift
+++ Sora/Views/MediaInfoView/MediaInfoView.swift
@@ -490,10 +490,15 @@ struct MediaInfoView: View {
     
     func playStream(url: String, fullURL: String, subtitles: String? = nil) {
         DispatchQueue.main.async {
-            guard let streamURL = URL(string: url) else { return }
-            let subtitleFileURL = subtitles != nil ? URL(string: subtitles!) : nil
-            DownloadManager.shared.downloadAndConvertHLS(from: streamURL, title: title, episode: selectedEpisodeNumber, subtitleURL: subtitleFileURL, sourceName: module.metadata.sourceName) { success, fileURL in
+            guard let streamURL = URL(string: url) else {
+                Logger.shared.log("Invalid stream URL: \(url)", type: "Error")
+                handleStreamFailure()
                 return
+            }
+            let subtitleFileURL = subtitles != nil ? URL(string: subtitles!) : nil
+            
+            DownloadManager.shared.downloadAndConvertHLS(from: streamURL, title: title, episode: selectedEpisodeNumber, subtitleURL: subtitleFileURL, sourceName: module.metadata.sourceName) { success, fileURL in
+                
             }
             
             let externalPlayer = UserDefaults.standard.string(forKey: "externalPlayer") ?? "Default"
