0b9dae6b98b04e600bd049de2e4c344833b8d2d7
Francesco <100066266+cranci1@users.noreply.github.com>
2025-03-26 15:06:10 +0100

    Revert "yeah idk tf is this"
    This reverts commit 5826f89866103583a8aad1b5708979c2046451dd.

--- Sora/ContentView.swift
+++ Sora/ContentView.swift
@@ -9,28 +9,20 @@ import SwiftUI
 import Kingfisher
 
 struct ContentView: View {
-    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding: Bool = false
-
     var body: some View {
-        Group {
-            if !hasCompletedOnboarding {
-                OnboardingView(hasCompletedOnboarding: $hasCompletedOnboarding)
-            } else {
-                TabView {
-                    LibraryView()
-                        .tabItem {
-                            Label("Library", systemImage: "books.vertical")
-                        }
-                    SearchView()
-                        .tabItem {
-                            Label("Search", systemImage: "magnifyingglass")
-                        }
-                    SettingsView()
-                        .tabItem {
-                            Label("Settings", systemImage: "gear")
-                        }
+        TabView {
+            LibraryView()
+                .tabItem {
+                    Label("Library", systemImage: "books.vertical")
                 }
-            }
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
         }
     }
 }
--- Sora/Views/SettingsView/SettingsSubViews/SettingsViewGeneral.swift
+++ Sora/Views/SettingsView/SettingsSubViews/SettingsViewGeneral.swift
@@ -70,6 +70,7 @@ struct SettingsViewGeneral: View {
                             }
                         }
                     }
+                    
                 }
             }
             
