ad96b5fdaad421338ee619be8f1ebe4e58b456e6
Seiike <122684677+Seeike@users.noreply.github.com>
2025-03-26 21:54:16 +0100

    changed it to series
    https://discord.com/channels/1293430817841741899/1318240587886891029/1354558523824144504

--- Sora/Views/MediaInfoView/MediaInfoView.swift
+++ Sora/Views/MediaInfoView/MediaInfoView.swift
@@ -296,7 +296,7 @@ struct MediaInfoView: View {
                                                     UserDefaults.standard.set(99999999.0, forKey: "totalTime_\(href)")
                                                 }
                                                 refreshTrigger.toggle()
-                                                Logger.shared.log("Marked \(ep.number - 1) episodes watched within anime \"\(title)\".", type: "General")
+                                                Logger.shared.log("Marked \(ep.number - 1) episodes watched within series \"\(title)\".", type: "General")
                                             }
                                         )
                                         .id(refreshTrigger)
