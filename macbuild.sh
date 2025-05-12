631b99ab01942b441dff4e6a45c020a2afdbf3cb
Francesco <100066266+cranci1@users.noreply.github.com>
2025-05-12 19:05:07 +0200

    macOS build workflow too now

---
+++ macbuild.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+WORKING_LOCATION="$(pwd)"
+APPLICATION_NAME=Sulfur
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+
+xcodebuild -project "$WORKING_LOCATION/$APPLICATION_NAME.xcodeproj" \
+    -scheme "$APPLICATION_NAME" \
+    -configuration Release \
+    -derivedDataPath "$WORKING_LOCATION/build/DerivedDataApp" \
+    -destination 'platform=macOS,variant=Mac Catalyst' \
+    clean build \
+    CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+
+DD_APP_PATH="$WORKING_LOCATION/build/DerivedDataApp/Build/Products/Release-maccatalyst/$APPLICATION_NAME.app"
+TARGET_APP="$WORKING_LOCATION/build/$APPLICATION_NAME.app"
+
+cp -r "$DD_APP_PATH" "$TARGET_APP"
+
+codesign --remove "$TARGET_APP"
+if [ -e "$TARGET_APP/_CodeSignature" ]; then
+    rm -rf "$TARGET_APP/_CodeSignature"
+fi
+
+echo "Mac Catalyst build completed: $TARGET_APP"
