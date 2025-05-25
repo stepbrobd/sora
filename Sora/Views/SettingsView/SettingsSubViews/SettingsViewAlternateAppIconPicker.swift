fe097bfd145f5b085a74f4c214e0e202709bbd04
Francesco <100066266+cranci1@users.noreply.github.com>
2025-05-25 13:02:04 +0200

    better now

--- Sora/Views/SettingsView/SettingsSubViews/SettingsViewAlternateAppIconPicker.swift
+++ Sora/Views/SettingsView/SettingsSubViews/SettingsViewAlternateAppIconPicker.swift
@@ -60,9 +60,9 @@ struct SettingsViewAlternateAppIconPicker: View {
     private func setAppIcon(named iconName: String) {
         if UIApplication.shared.supportsAlternateIcons {
             UIApplication.shared.setAlternateIconName(iconName == "Default" ? nil : "AppIcon_\(iconName)", completionHandler: { error in
-                isPresented = false
                 if let error = error {
                     Logger.shared.log("Failed to set alternate icon: \(error.localizedDescription)", type: "Error")
+                    isPresented = false
                 }
             })
         }
