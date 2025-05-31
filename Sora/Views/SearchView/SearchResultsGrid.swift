abbbdc00ee2241c2504fc496cec399cfb101d94b
Francesco <100066266+cranci1@users.noreply.github.com>
2025-05-31 17:59:37 +0200

    idk 😭

--- Sora/Views/SearchView/SearchResultsGrid.swift
+++ Sora/Views/SearchView/SearchResultsGrid.swift
@@ -28,47 +28,50 @@ struct SearchResultsGrid: View {
     }
     
     var body: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 12), count: columnsCount), spacing: 12) {
-            ForEach(items) { item in
-                NavigationLink(destination: MediaInfoView(title: item.title, imageUrl: item.imageUrl, href: item.href, module: selectedModule)) {
-                    ZStack {
-                        KFImage(URL(string: item.imageUrl))
-                            .resizable()
-                            .aspectRatio(0.72, contentMode: .fill)
-                            .frame(width: cellWidth, height: cellWidth * 1.5)
-                            .cornerRadius(12)
-                            .clipped()
-                        
-                        VStack {
-                            Spacer()
-                            HStack {
-                                Text(item.title)
-                                    .lineLimit(2)
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(.leading)
+        ScrollView {
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 12), count: columnsCount), spacing: 12) {
+                ForEach(items) { item in
+                    NavigationLink(destination: MediaInfoView(title: item.title, imageUrl: item.imageUrl, href: item.href, module: selectedModule)) {
+                        ZStack {
+                            KFImage(URL(string: item.imageUrl))
+                                .resizable()
+                                .aspectRatio(0.72, contentMode: .fill)
+                                .frame(width: cellWidth, height: cellWidth * 1.5)
+                                .cornerRadius(12)
+                                .clipped()
+                            
+                            VStack {
                                 Spacer()
-                            }
-                            .padding(12)
-                            .background(
-                                LinearGradient(
-                                    colors: [
-                                        .black.opacity(0.7),
-                                        .black.opacity(0.0)
-                                    ],
-                                    startPoint: .bottom,
-                                    endPoint: .top
+                                HStack {
+                                    Text(item.title)
+                                        .lineLimit(2)
+                                        .foregroundColor(.white)
+                                        .multilineTextAlignment(.leading)
+                                    Spacer()
+                                }
+                                .padding(12)
+                                .background(
+                                    LinearGradient(
+                                        colors: [
+                                            .black.opacity(0.7),
+                                            .black.opacity(0.0)
+                                        ],
+                                        startPoint: .bottom,
+                                        endPoint: .top
+                                    )
+                                        .shadow(color: .black, radius: 4, x: 0, y: 2)
                                 )
-                                    .shadow(color: .black, radius: 4, x: 0, y: 2)
-                            )
+                            }
+                            .frame(width: cellWidth)
                         }
-                        .frame(width: cellWidth)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .padding(4)
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .padding(4)
                 }
             }
+            .padding(.top)
+            .padding()
         }
-        .padding(.top)
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
