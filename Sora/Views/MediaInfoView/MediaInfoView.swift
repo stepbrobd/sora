96304b21c512eedb7197fb5bf4feff0afd1c39e3
cranci <100066266+cranci1@users.noreply.github.com>
2025-06-03 16:16:02 +0200

    Update MediaInfoView.swift

--- Sora/Views/MediaInfoView/MediaInfoView.swift
+++ Sora/Views/MediaInfoView/MediaInfoView.swift
@@ -1966,7 +1966,7 @@ struct MediaInfoView: View {
         
         let detachedWindow = UIWindow(windowScene: windowScene)
         let hostingController = UIViewController()
-        hostingController.view.backgroundColor = .clear
+        hostingController.view.backgroundColor = .black
         detachedWindow.rootViewController = hostingController
         detachedWindow.backgroundColor = .clear
         detachedWindow.windowLevel = .normal + 1
@@ -1981,7 +1981,7 @@ struct MediaInfoView: View {
         
         let detachedWindow = UIWindow(windowScene: windowScene)
         let hostingController = UIViewController()
-        hostingController.view.backgroundColor = .clear
+        hostingController.view.backgroundColor = .black
         detachedWindow.rootViewController = hostingController
         detachedWindow.backgroundColor = .clear
         detachedWindow.windowLevel = .normal + 1
