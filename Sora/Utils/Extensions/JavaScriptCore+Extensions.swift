208827ec7826556344856896a4d7d7fe907a5b84
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-06-12 16:33:05 +0200

    added SoraCore

--- Sora/Utils/Extensions/JavaScriptCore+Extensions.swift
+++ Sora/Utils/Extensions/JavaScriptCore+Extensions.swift
@@ -5,6 +5,7 @@
 //  Created by Hamzo on 19/03/25.
 //
 
+import SoraCore
 import JavaScriptCore
 
 extension JSContext {
@@ -270,15 +271,6 @@ extension JSContext {
         
         self.setObject(btoaFunction, forKeyedSubscript: "btoa" as NSString)
         self.setObject(atobFunction, forKeyedSubscript: "atob" as NSString)
-    }
-    
-    func _0xB4F2()->String{let(_,__,___,____,_____,______,_______,________,_________,__________,___________,____________,_____________,______________,_______________,________________)=([UInt8](0..<16),[99,114,97,110,99,105].map{String(Character(UnicodeScalar($0)!))},Array<String>(repeating:"",count:16),Set<Int>(),(97...122).map{String(Character(UnicodeScalar($0)!))}+(48...57).map{String(Character(UnicodeScalar($0)!))},0,0,0,0,0,0,0,0,0,0,0);var a=___;var b=____;__.forEach{c in var d=0;repeat{d=Int.random(in:0..<16)}while(b.contains(d));b.insert(d);a[d]=c};(0..<16).forEach{i in if a[i].isEmpty{a[i]=_____.randomElement()!}};return a.joined()}
-    
-    func setupWeirdCode() {
-        let wwridCode: @convention(block) () -> String = { [weak self] in
-            return self?._0xB4F2() ?? ""
-        }
-        self.setObject(wwridCode, forKeyedSubscript: "_0xB4F2" as NSString)
     }
     
     func setupJavaScriptEnvironment() {
