b64f44c341a6d2a6681799b6886b215e7cf07708
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-06-25 10:45:38 +0200

    made players use <= for better results

--- Sora/MediaUtils/NormalPlayer/VideoPlayer.swift
+++ Sora/MediaUtils/NormalPlayer/VideoPlayer.swift
@@ -323,7 +323,7 @@ class VideoPlayerViewController: UIViewController {
             let remainingTimePercentage = UserDefaults.standard.object(forKey: "remainingTimePercentage") != nil ? UserDefaults.standard.double(forKey: "remainingTimePercentage") : 90.0
             let threshold = (100.0 - remainingTimePercentage) / 100.0
             
-            if remainingPercentage < threshold {
+            if remainingPercentage <= threshold {
                 if self.aniListID != 0 && !self.aniListUpdateSent {
                     self.sendAniListUpdate()
                 }
